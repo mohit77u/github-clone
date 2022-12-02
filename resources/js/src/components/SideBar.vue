a8aca2fa7e870ab9c9554ebf36ec3e13ded3c78c
haw-ms <ms@webimpact.co.in>
2022-12-02 13:20:13 +0530

    Update

--- resources/js/src/components/SideBar.vue
+++ resources/js/src/components/SideBar.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="sidebar md:w-[350px] w-full md:sticky static md:left-0 md:top-0 md:z-40 md:min-h-[50vh] md:max-h-[100vh] bg-[#0d1117] border-r border-gray-700 py-5 px-8 overflow-y-auto">
         <div class="top flex flex-wrap items-center justify-between">
-            <h3 class="text-md text-gray-300">Top Repository</h3>
-            <button class="bg-green-primary px-2 py-1 text-sm rounded text-white flex items-center justify-center gap-x-1">
+            <h3 class="text-md font-semibold text-gray-300">Top Repository</h3>
+            <button class="bg-green-primary px-3 py-1.5 text-sm rounded-md text-white flex items-center justify-center gap-x-1">
                 <svg aria-hidden="true" fill="white" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-repo">
                     <path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"></path>
                 </svg>
@@ -10,14 +10,14 @@
             </button>
         </div>
         <!-- search input -->
-        <input type="text" placeholder="Find repository..." class="my-4 border border-gray-700 focus:border-blue-400 focus:outline focus:ring-blue-400 px-3 py-1 text-sm rounded w-full bg-dark-main text-gray-300">
+        <input type="text" placeholder="Find repository..." class="my-2 border border-gray-700 focus:border-blue-400 focus:outline focus:ring-blue-400 px-3 py-1 text-sm rounded w-full bg-dark-main text-gray-300">
 
         <!-- repositories data -->
-        <div class="repositories pb-12">
+        <div class="repositories pt-2 pb-12">
             <div class="repo py-1.5" v-for="(repo, index) in repositories" :key="index">
                 <a href="#" class="flex items-center">
                     <img :src="repo.owner.avatar_url" :alt="repo.name" class="w-5 h-5 rounded-full">
-                    <h4 class="text-gray-300 text-sm ml-2">{{repo.full_name}}</h4>
+                    <h4 class="text-gray-300 text-[14px] ml-2">{{repo.full_name}}</h4>
                 </a>
             </div>
 
--- resources/js/src/pages/Home.vue
+++ resources/js/src/pages/Home.vue
@@ -10,9 +10,34 @@
 
             <!-- main -->
             <div class="main-right-content p-4 bg-dark-main">
-                <h1 class="text-white">Hello</h1>
+                <h1 class="text-white">Content</h1>
             </div>
         </section>
+
+        <!-- connect gitHub popup -->
+        <div class="connect-popup fixed top-0 left-0 z-40 bg-black/70 w-full h-full flex justify-center items-center" v-if="connect">
+            <div class="popup-main max-w-[350px] min-w-[350px] rounded border border-gray-700 bg-[#0d1117] py-5">
+                <div class="popup-head px-5 flex justify-between items-center">
+                    <h2 class="text-white text-sm pb-1.5">Connect GitHub</h2>
+                    <!-- <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="text-gray-400 hover:text-blue-400" fill="#ddd">
+                        <path fill-rule="evenodd" d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"></path>
+                    </svg> -->
+                </div>
+                <div class="popup-top-bar mt-4 w-full p-4 border-t border-b border-[rgba(187,128,9,0.4)] bg-gradient-to-b from-[rgba(187,128,9,0.15)] to-[rgba(187,128,9,0.15)]">
+                    <p class="text-gray-400 text-sm">Connect your GitHub account now.</p>
+                </div>
+                <div class="popup-main px-5">
+                    <p class="text-gray-300 text-sm pt-4">Enter your GitHub username to connect with your account, and you can perform any actions that will reflect to your GitHub account.</p>
+                    <form class="py-4">
+                        <div class="form-group">
+                            <label class="block mb-1 text-gray-300 text-sm">Please type your GitHub username</label>
+                            <input type="text" placeholder="" class="my-2 border border-gray-700 focus:border-blue-400 focus:outline focus:ring-blue-400 px-3 py-1 text-sm rounded w-full bg-dark-main text-gray-300">
+                            <button class="bg-transparent hover:bg-green-primary border border-gray-700 w-full px-3 py-1.5 text-sm rounded-md text-green-primary hover:text-white flex items-center justify-center gap-x-1 font-medium">Connect GitHub</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -28,7 +53,7 @@ export default {
     },
     data(){
         return{
-            user: '',
+            connect: false,
             githubUser: '',
         }
     },
@@ -45,7 +70,11 @@ export default {
             }
             axios.get('/api/user', config)
             .then((res) => {
-                this.user = res.data.user
+                // this.user = res.data.user
+                const username = res.data.user.username
+                if(username == null || username == ''){
+                    this.connect = true
+                }
                 this.getGithubUser(res.data.user.username);
             }).catch((err) => {
                 console.log(err)
